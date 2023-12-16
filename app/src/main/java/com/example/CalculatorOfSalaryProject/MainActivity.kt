d8097496b3e825167021b242b78a16dcb0efd829
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-12-16 17:04:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/CalculatorOfSalaryProject/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.CalculatorOfSalaryProject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.example.CalculatorOfSalaryProject.ui.theme.SalaryCalculatorJPCTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.CalculatorOfSalaryProject.screen_1.Screen1
+import com.example.CalculatorOfSalaryProject.screen_2.Screen2
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SalaryCalculatorJPCTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "screen1"
+                    ) {
+                        composable("screen1") {
+                            Screen1 {
+                                Bundle().apply {
+                                    putFloat("salary", it)}
+                                navController.navigate("screen2/$it")
+                            }
+                        }
+                            composable(
+                            "screen2/{salary}",
+                            arguments = listOf(navArgument("salary") { type = NavType.FloatType })
+                        ) { backStackEntry ->
+                            val salary = backStackEntry.arguments?.getFloat("salary") ?: 0.0f
+                            Screen2(salary = salary.toDouble()) {
+                                navController.navigateUp()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
